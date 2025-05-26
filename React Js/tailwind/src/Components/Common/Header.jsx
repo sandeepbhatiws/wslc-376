b195ca39596edf79d5e54c2fc9f536ecae9411a6
pradeeprai1990 <pradeep.9997@gmail.com>
2025-05-26 20:05:53 +0530

    Changes

--- React Js/tailwind/src/Components/Common/Header.jsx
+++ React Js/tailwind/src/Components/Common/Header.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { CiShoppingCart } from "react-icons/ci";
+import { Link } from 'react-router-dom'
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -93,27 +89,26 @@ export default function Header() {
             </div>
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex shrink-0 items-center">
-                <img
+                <Link to="/">
+                  <img
                   alt="Your Company"
                   src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
                   className="h-8 w-auto"
                 />
+                </Link>
+                
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      aria-current={item.current ? 'page' : undefined}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'rounded-md px-3 py-2 text-sm font-medium',
-                      )}
+                  
+                    <Link
+                      key=""
+                      to="/products"
+                      className=' text-white'
                     >
-                      {item.name}
-                    </a>
-                  ))}
+                       All Products
+                    </Link>
+
                 </div>
               </div>
             </div>
@@ -122,9 +117,10 @@ export default function Header() {
                 type="button"
                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
               >
-                <span className="absolute -inset-1.5" />
-                <span className="sr-only">View notifications</span>
-                <BellIcon aria-hidden="true" className="size-6" />
+                <Link to="/view-carts">
+                  <CiShoppingCart aria-hidden="true" className="size-6" />
+                </Link>
+                
               </button>
 
               {/* Profile dropdown */}
@@ -176,20 +172,15 @@ export default function Header() {
 
         <DisclosurePanel className="sm:hidden">
           <div className="space-y-1 px-2 pt-2 pb-3">
-            {navigation.map((item) => (
+            
               <DisclosureButton
-                key={item.name}
                 as="a"
-                href={item.href}
-                aria-current={item.current ? 'page' : undefined}
-                className={classNames(
-                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'block rounded-md px-3 py-2 text-base font-medium',
-                )}
+                href=""
+                className='bg-gray-900 text-white'
               >
-                {item.name}
+                Products
               </DisclosureButton>
-            ))}
+    
           </div>
         </DisclosurePanel>
       </Disclosure>
--- React Js/tailwind/src/main.jsx
+++ React Js/tailwind/src/main.jsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from './Components/HomePage';
 import RootLayout from './Components/RootLayout';
 import AboutUsPage from './Components/AboutUsPage';
+import ProductLisitng from './Components/ProductLisitng';
+import ProductDetails from './Components/ProductDetails';
+import ViewCart from './Components/ViewCart';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -14,6 +17,9 @@ createRoot(document.getElementById('root')).render(
       <Route element={ <RootLayout/> }>
         <Route path="/" element={<HomePage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/products/:slug?" element={<ProductLisitng />} />
+        <Route path="/product-details/:id" element={<ProductDetails />} />
+        <Route path="/view-carts" element={<ViewCart />} />
       </Route>
       
     </Routes>
