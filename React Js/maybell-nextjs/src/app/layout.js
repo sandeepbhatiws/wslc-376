aeb7e9d20513d7ff106b3f4d2e5ff356d9def1f1
pradeeprai1990 <pradeep.9997@gmail.com>
2025-05-30 20:00:57 +0530

    Changes

---
+++ React Js/maybell-nextjs/src/app/layout.js
@@ -0,0 +1,12 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className=""
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
