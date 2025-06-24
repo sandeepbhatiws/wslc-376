2467ea7ed73b63089518216986cf2e93e023eac0
pradeeprai1990 <pradeep.9997@gmail.com>
2025-06-24 21:02:27 +0530

    Chanegs

--- Backend/mongodb-api/index.js
+++ Backend/mongodb-api/index.js
@@ -3,6 +3,12 @@ const mongodb = require('mongodb');
 const server = express(); // To Create Exucatable Function
 const dbConnection = require('./dbConfig/databaseConnection.js');
 
+// parse requests of content-type - application/json
+server.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+server.use(express.urlencoded({ extended: true }));
+
 server.get('/',(request, response) => {
     response.end('Server is working fine !');
 })
@@ -83,6 +89,125 @@ server.get('/view-categories', async(request, response) => {
         response.send(output);
     })
 
+})
+
+server.post('/api/add-material', async(request, response) => {
+    const db = await dbConnection();  // Execuatble Function
+    db.collection('materials').insertOne({
+        name : request.body.name,
+        order : request.body.order
+    }).then((result) => {
+        const output = {
+            _status : true,
+            _message : 'Record created succussfully !',
+            _data : result
+        }
+
+        response.send(output);
+    }).catch(() => {
+        const output = {
+            _status : false,
+            _message : 'Something went wrong !',
+            _data : null
+        }
+
+        response.send(output);
+    })
+})
+
+server.post('/api/view-material', async(request, response) => {
+    const db = await dbConnection();
+
+    if(request.body.name){
+        var filter = {
+            name : request.body.name
+        }
+    } else {
+        var filter = {};
+    }
+
+    db.collection('materials').find(filter).toArray()
+    .then((result) => {
+        if(result.length > 0){
+            const output = {
+                _status : true,
+                _message : 'Record fetch succussfully !',
+                _data : result
+            }
+
+            response.send(output);
+        } else {
+            const output = {
+                _status : false,
+                _message : 'No record found !',
+                _data : result
+            }
+
+            response.send(output);
+        }
+    })
+    .catch(() => {
+        const output = {
+            _status : false,
+            _message : 'Something went wrong !',
+            _data : []
+        }
+
+        response.send(output);
+    })
+})
+
+server.put('/api/update-material/:id', async(request, response) => {
+    const db = await dbConnection();  // Execuatble Function
+
+    db.collection('materials').updateOne({
+        _id : new mongodb.ObjectId(request.params.id)
+    },{
+        $set : {
+            name : request.body.name,
+            order : request.body.order
+        }
+    }).then((result) => {
+        const output = {
+            _status : true,
+            _message : 'Record updated succussfully !',
+            _data : result
+        }
+
+        response.send(output);
+    }).catch(() => {
+        const output = {
+            _status : false,
+            _message : 'Something went wrong !',
+            _data : null
+        }
+
+        response.send(output);
+    })
+})
+
+server.delete('/api/delete-material', async(request, response) => {
+    const db = await dbConnection();  // Execuatble Function
+
+    db.collection('materials').deleteOne({
+        _id : new mongodb.ObjectId(request.body.id)
+    }).then((result) => {
+        const output = {
+            _status : true,
+            _message : 'Record delete succussfully !',
+            _data : result
+        }
+
+        response.send(output);
+    }).catch(() => {
+        const output = {
+            _status : false,
+            _message : 'Something went wrong !',
+            _data : null
+        }
+
+        response.send(output);
+    })
 })
 
 server.listen(5000, () => {
