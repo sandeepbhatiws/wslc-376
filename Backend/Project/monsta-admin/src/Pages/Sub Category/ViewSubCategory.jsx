41fdd017d066aee28513bac3972102d0dcd8030e
pradeeprai1990 <pradeep.9997@gmail.com>
2025-07-26 20:45:22 +0530

    Changes

--- Backend/Project/monsta-admin/src/Pages/Sub Category/ViewSubCategory.jsx
+++ Backend/Project/monsta-admin/src/Pages/Sub Category/ViewSubCategory.jsx
@@ -1,16 +1,149 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Breadcrumb from '../../common/Breadcrumb'
 import { Link } from 'react-router-dom';
 import { MdFilterAltOff, MdModeEdit, MdModeEditOutline } from 'react-icons/md';
 import { CiEdit } from 'react-icons/ci';
 import { FaFilter } from 'react-icons/fa';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 // import { MdModeEditOutline } from "react-icons/md";
+import ResponsivePagination from 'react-responsive-pagination';
+import 'react-responsive-pagination/themes/classic-light-dark.css';
 
 export default function ViewCategory() {
-  // let [orderModal, setOrderModal] = useState(false);
 
   let [activeFilter, setactiveFilter] = useState(true);
-  let [activeDropDown, setactiveDropDown] = useState(false);
+  let [nameFilter, setNameFilter] = useState('');
+  let [parentCategoryFilter, setParentCategoryFilter] = useState('');
+  const [subCategories, setSubCategories] = useState([]);
+
+  let [checkedValue, setCheckedValue] = useState([]);
+  let [apiStatus, setApiStatus] = useState(false);
+  let [imagePath, setImagePath] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.post(import.meta.env.VITE_API_BASE_URL + import.meta.env.VITE_CATEGORY_VIEW, {
+      limit : 100
+    })
+      .then((response) => {
+        if (response.data._status == true) {
+          setCategories(response.data._data)
+        } else {
+          setCategories([]);
+        }
+      })
+      .catch(() => {
+        toast.error('Something went wrong !!');
+      })
+  }, []);
+
+  useEffect(() => {
+    axios.post(import.meta.env.VITE_API_BASE_URL + import.meta.env.VITE_SUB_CATEGORY_VIEW, {
+      name: nameFilter,
+      parent_category_id: parentCategoryFilter,
+      page: currentPage
+    })
+      .then((response) => {
+        if (response.data._status == true) {
+          setImagePath(response.data._image_path);
+          setSubCategories(response.data._data)
+          setTotalPages(response.data._pagination.total_pages)
+        } else {
+          setSubCategories([]);
+        }
+      })
+      .catch(() => {
+        toast.error('Something went wrong !!');
+      })
+  }, [nameFilter, parentCategoryFilter, apiStatus, currentPage]);
+
+  var singleChecked = (id) => {
+    if(checkedValue.includes(id)){
+      var data = checkedValue.filter((value) => {
+        if(value != id){
+          return value;
+        }
+      })
+      data = [...data];
+      setCheckedValue(data);
+    } else {
+      const data = [...checkedValue, id];
+      setCheckedValue(data);
+    }
+  }
+
+  var checkedAll  = () => {
+    if(subCategories.length != checkedValue.length){
+      var data = [];
+      subCategories.forEach((value) => {
+        data.push(value._id);
+      })
+      setCheckedValue([...data]);
+    } else {
+      setCheckedValue([]);
+    }
+  }
+
+  const changeStatus = () => {
+    if(checkedValue.length > 0){
+      axios.put('http://localhost:8000/api/admin/sub-categories/change-status', {
+        id : checkedValue
+      })
+      .then((response) => {
+        if (response.data._status == true) {
+          toast.success(response.data._message);
+          setApiStatus(!apiStatus);
+          setCheckedValue([]);
+        } else {
+          toast.error(response.data._message);
+        }
+      })
+      .catch(() => {
+        toast.error('Something went wrong !!');
+      })
+    } else {
+      toast.error('Please select atlest 1 record to change status')
+    }
+
+    
+  }
+
+  const deleteRecords = () => {
+    if(checkedValue.length > 0){
+      if(confirm('Are you sure you want to delete ?')){
+          axios.put('http://localhost:8000/api/admin/sub-categories/delete', {
+            id : checkedValue
+          })
+          .then((response) => {
+            if (response.data._status == true) {
+              toast.success(response.data._message);
+              setApiStatus(!apiStatus);
+              setCheckedValue([]);
+            } else {
+              toast.error(response.data._message);
+            }
+          })
+          .catch(() => {
+            toast.error('Something went wrong !!');
+          })
+      }
+    } else {
+      toast.error('Please select atlest 1 record to delete')
+    }
+  }
+
+  // Filter parent category
+  const filterParentCategory = (value) => {
+    setParentCategoryFilter(value.target.value);
+  }
+
+  const filterName = (value) => {
+    setNameFilter(value.target.value);
+  }
+
   return (
     <section className="w-full">
 
@@ -22,17 +155,24 @@ export default function ViewCategory() {
           <div className="">
 
             <select
+              onChange={ filterParentCategory }
               name="parentCatSelectBox"
               className="border  border-gray-300 text-gray-900  text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-3"
             >
               <option value="">Select Parent Category</option>
-              <option value="Mens">Men's</option>
-              <option value="Women">Women</option>
-              <option value="Sale">Sale</option>
+
+              {
+                categories.map((value) => {
+                  return(
+                    <option value={value._id}>{value.name}</option>
+                  )
+                })
+              }
             </select>
           </div>
           <div className="">
             <input
+              onKeyUp={ filterName }
               type="text"
               id="simple-search"
               className="border  border-gray-300 text-gray-900  text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-3"
@@ -40,34 +180,7 @@ export default function ViewCategory() {
               required
             />
           </div>
-          <div className=''>
-            <button
-              type="submit"
-              className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span className="sr-only">Search</span>
-            </button>
-          </div>
-
-
-
         </form>
-
 
       </div>
       <div className="w-full min-h-[610px]">
@@ -81,8 +194,14 @@ export default function ViewCategory() {
                 {activeFilter ? <FaFilter className='text-[18px]' /> : <MdFilterAltOff className='text-[18px]' />}
               </div>
 
-              <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> Change Status</button>
-              <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete </button>
+              <button
+              onClick={ changeStatus }
+              type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"> Change Status</button>
+
+
+              <button
+              onClick={ deleteRecords }
+              type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete </button>
             </div>
           </div>
           <div className="border border-t-0 rounded-b-md border-slate-400">
@@ -98,17 +217,20 @@ export default function ViewCategory() {
                     <tr>
                       <th scope="col" class="p-4">
                         <div class="flex items-center">
-                          <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                          <input
+                            onClick={ checkedAll }
+                            checked={ subCategories.length == checkedValue.length ? 'checked' : '' }
+                          id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                           <label for="checkbox-all-search" class="sr-only">checkbox</label>
                         </div>
                       </th>
                       <th scope="col" class="px-6 py-3">
-                      Parent Category Name
+                        Parent Category Name
                       </th>
                       <th scope="col" class="px-0 py-3">
                         Sub Category Name
                       </th>
-                      
+
                       <th scope="col" class=" w-[12%] ">
                         Image
                       </th>
@@ -124,54 +246,85 @@ export default function ViewCategory() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr class="bg-white  dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                      <td class="w-4 p-4">
-                        <div class="flex items-center">
-                          <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                          <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                        </div>
-                      </td>
-                      <td scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                    {
+                      subCategories.length > 0
+                        ?
 
-                        <div class="py-4">
-                          Shoe
+                        subCategories.map((v, i) => {
+                          return (
+                            <tr key={i} class="bg-white  dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                              <td class="w-4 p-4">
+                                <div class="flex items-center">
+                                  <input
+                                    onClick={() => singleChecked(v._id)}
 
-                        </div>
-                      </td>
-                      <td class=" py-4">
-                        Men
+                                    checked={checkedValue.includes(v._id) ? 'checked' : ''}
 
-                      </td>
+                                    id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                  <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                </div>
+                              </td>
+                              <td class=" py-4">
+                                {v.parent_category_id}
+                              </td>
+                              <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
 
-                      <td class=" py-4">
-                        <img class="w-10 h-10 rounded-full" src="https://packshifts.in/images/iso.png" alt="Jese image" />
-                      </td>
-                      <td class=" py-4">
-                        1
-                      </td>
-                      <td class=" py-4">
+                                <div class="py-4">
+                                  <div class="text-base font-semibold">{v.name}</div>
+                                </div>
+                              </th>
+                              <td class=" py-4">
+                                {
+                                  v.image != ''
+                                    ?
+                                    <img class="w-10 h-10 rounded-full" src={imagePath + v.image} alt="Jese image" />
+                                    :
+                                    'N/A'
+                                }
 
-                        <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center me-2 mb-2">Active</button>
-                      </td>
-                      <td class=" py-4">
+                              </td>
+                              <td class=" py-4">
+                                {v.order}
+                              </td>
+                              <td class=" py-4">
+                                {
+                                  v.status == 1
+                                    ?
+                                    <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center me-2 mb-2">Active</button>
+                                    :
+                                    <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-1.5 text-center me-2 mb-2">Deactive</button>
+                                }
+                              </td>
+                              <td class=" py-4">
 
-                        <Link to={`/category/sub-category/update/${2222}`} >
-                          <div className="rounded-[50%] w-[40px] h-[40px] flex items-center justify-center text-white bg-blue-700  border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <MdModeEdit className='text-[18px]' />
-                          </div>
-                        </Link>
-
-                      </td>
-                    </tr>
-
-
-
+                                <Link to={`/category/update/${v._id}`} >
+                                  <div className="rounded-[50%] w-[40px] h-[40px] flex items-center justify-center text-white bg-blue-700  border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <MdModeEdit className='text-[18px]' />
+                                  </div>
+                                </Link>
+                              </td>
+                            </tr>
+                          )
+                        })
 
 
+                        :
+                        <tr class="bg-white  dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+
+                          <td class=" py-4 text-center fw-bold" colSpan={6}>
+                            <b>No Record Found !!</b>
+                          </td>
+                        </tr>
+                    }
                   </tbody>
                 </table>
               </div>
-
+                
+                <ResponsivePagination
+                current={currentPage}
+                total={totalPages}
+                onPageChange={setCurrentPage}
+              />
 
             </div>
 
